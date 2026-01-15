b96334e77a4e484b489c533a1f6be53c670317e1
ditto-chaar <md.aqeelahmed007@gmail.com>
2026-01-15 22:33:20 +0530

    feat: implement user login and JWT authentication

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,4 +1,5 @@
 import User from "../models/User.js"
+import generateToken from "../utils/generateToken.js"
 
 export const registerUser = async (req,res)=>{
   const {name,email,password} = req.body
@@ -20,16 +21,44 @@ export const registerUser = async (req,res)=>{
       return res.status(201).json({
         _id: newUser._id,
         name: newUser.name,
-        email: newUser.email
+        email: newUser.email,
+        token:generateToken(newUser._id)
       })
+
      }else{
       return res.status(400).json({
         message: 'invalid user data'
       })
      }
-     
+
   }catch(error){
     console.log(error);
     res.status(500).json({ message: 'server error'})
   }
+}
+
+
+export async function authUser(req,res){
+  const {email, password} = req.body
+
+  try{
+    const user = await User.findOne({email}).select("+password")
+
+    if(user && (await user.matchPassword(password))){
+      res.status(200).json({
+        token: generateToken(user._id)
+      })
+    }else {
+      return res.status(401).json({
+        message:"invalid email or password"
+      })
+    }
+    
+  }catch(error){
+    console.log(error);
+    res.status(500).json({
+      message:"server error"
+    })
+  }
+  
 }
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,8 +1,10 @@
 import express from 'express';
-import { registerUser } from '../controllers/userController.js';
+import { authUser, registerUser } from '../controllers/userController.js';
 
 const router = express.Router();
 
 router.post('/', registerUser);
+
+router.post('/auth',authUser)
 
 export default router;
